a7dbb433a806cd0360f6e485d433d5873f3c6be5
Daniel Nash <thedash52@hotmail.com>
2017-05-18 22:09:44 +1200

    started working on the LoginView and resource dictionary

--- votingFrontend/votingFrontend/ViewModels/LoginViewModel.cs
+++ votingFrontend/votingFrontend/ViewModels/LoginViewModel.cs
@@ -2,13 +2,187 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.Resources;
 
 namespace votingFrontend.ViewModels
 {
     public class LoginViewModel : INotifyPropertyChanged
     {
-        public event PropertyChangedEventHandler PropertyChanged;
+        private string titleText;
+        private string firstNameText;
+        private string firstName;
+        private string lastNameText;
+        private string lastName;
+        private string dobText;
+        private DateTime dob;
+        private string electoralIdText;
+        private string electoralId;
+        private string loginText;
+
+        private ResourceLoader resource;
+
+        public LoginViewModel()
+        {
+            resource = new ResourceLoader();
+            TitleText = resource.GetString("LoginTitle");
+            FirstNameText = resource.GetString("FirstName");
+            LastNameText = resource.GetString("LastName");
+            DoBText = resource.GetString("DateOfBirth");
+            ElectoralIdText = resource.GetString("ElectoralID");
+            LoginText = resource.GetString("LoginButton");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        public string TitleText
+        {
+            get
+            {
+                return this.titleText;
+            }
+
+            set
+            {
+                this.titleText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string FirstNameText
+        {
+            get
+            {
+                return this.firstNameText;
+            }
+
+            set
+            {
+                this.firstNameText = value;
+            }
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+
+            set
+            {
+                this.firstName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string LastNameText
+        {
+            get
+            {
+                return this.lastNameText;
+            }
+
+            set
+            {
+                this.lastNameText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+
+            set
+            {
+                this.lastName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string DoBText
+        {
+            get
+            {
+                return this.dobText;
+            }
+
+            set
+            {
+                this.dobText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DateTime DoB
+        {
+            get
+            {
+                return this.dob;
+            }
+
+            set
+            {
+                this.dob = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string ElectoralIdText
+        {
+            get
+            {
+                return this.electoralIdText;
+            }
+
+            set
+            {
+                this.electoralIdText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string ElectoralId
+        {
+            get
+            {
+                return this.electoralId;
+            }
+
+            set
+            {
+                this.electoralId = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string LoginText
+        {
+            get
+            {
+                return this.loginText;
+            }
+
+            set
+            {
+                this.loginText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            if (this.PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
     }
 }
